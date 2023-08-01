4923f8ac55cb679d2fc57ba165f45c6b4051b69e
Ryosuke Horiuchi <ryoiryojoho@gmail.com>
2023-08-01 11:43:55 +0900

    Unet w/ timm-resnest26d; EXP 1

--- src/gr-icrgw-training-with-4-folds/loss.py
+++ src/gr-icrgw-training-with-4-folds/loss.py
@@ -11,11 +11,13 @@ import yaml
 with open("config.yaml", "r") as file_obj:
     config = yaml.safe_load(file_obj)
 
-DiceLoss = smp.losses.DiceLoss(mode='binary')
+DiceLoss = smp.losses.DiceLoss(mode='binary', smooth=config["model"]["loss_smooth"])
 BCELoss = smp.losses.SoftBCEWithLogitsLoss()
 alpha = 0.5
 beta = 1 - alpha
 TverskyLoss = smp.losses.TverskyLoss(mode='binary', log_loss=False, alpha=alpha, beta=beta)
 
 def criterion(y_pred, y_true):
+    y_pred = y_pred.squeeze(1)
+    # print(f'y_pred.shape: {y_pred.shape}; y_true.shape: {y_true.shape}')
     return 0.3 * BCELoss(y_pred, y_true)  + 0.4 * DiceLoss(y_pred, y_true) + 0.3 * TverskyLoss(y_pred, y_true)
